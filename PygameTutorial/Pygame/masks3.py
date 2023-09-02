d3a703676a55f9b8ba6630cd6aa50a7bd17bd1fa
KoalaQuakes <flame.frost@gmail.com>
2023-09-02 23:40:58 +0200

    Outline method
    Adding a simple outline to sprites using masks and outline() method

--- PygameTutorial/Pygame/masks3.py
+++ PygameTutorial/Pygame/masks3.py
@@ -10,15 +10,15 @@ obstacle_pos = (100,100)
 obstacle_mask = pygame.mask.from_surface(obstacle_surf)
 
 # mask into surface
-new_obstacle_surf = obstacle_mask.to_surface()
-new_obstacle_surf.set_colorkey((0,0,0))
+#new_obstacle_surf = obstacle_mask.to_surface()
+#new_obstacle_surf.set_colorkey((0,0,0))
 
 # filling in the surface with a color
-surf_w,surf_h = new_obstacle_surf.get_size()
-for x in range(surf_w):
-    for y in range(surf_h):
-        if new_obstacle_surf.get_at((x,y))[0] != 0:
-            new_obstacle_surf.set_at((x,y),'orange')
+#surf_w,surf_h = new_obstacle_surf.get_size()
+#for x in range(surf_w):
+#    for y in range(surf_h):
+#        if new_obstacle_surf.get_at((x,y))[0] != 0:
+#            new_obstacle_surf.set_at((x,y),'orange')
 
 
 while True:
@@ -29,7 +29,14 @@ while True:
 
     screen.fill('grey')
     screen.blit(obstacle_surf,obstacle_pos)
-    screen.blit(new_obstacle_surf,obstacle_pos)
+    #screen.blit(new_obstacle_surf,obstacle_pos)
+
+    # a simple way to create an outline from a mask
+    for point in obstacle_mask.outline():
+        x = point[0] + obstacle_pos[0]
+        y = point[1] + obstacle_pos[1]
+        pygame.draw.circle(screen, 'red', (x,y), 3)
+
 
     pygame.display.update()
     clock.tick(60)
