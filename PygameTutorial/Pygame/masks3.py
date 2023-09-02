c3c07fca8cbb25ce2699d8077df8cf0679ba9ef8
KoalaQuakes <flame.frost@gmail.com>
2023-09-02 23:55:14 +0200

    Complex mask outline
    adding an offset behind the mask.

--- PygameTutorial/Pygame/masks3.py
+++ PygameTutorial/Pygame/masks3.py
@@ -10,15 +10,15 @@ obstacle_pos = (100,100)
 obstacle_mask = pygame.mask.from_surface(obstacle_surf)
 
 # mask into surface
-#new_obstacle_surf = obstacle_mask.to_surface()
-#new_obstacle_surf.set_colorkey((0,0,0))
+new_obstacle_surf = obstacle_mask.to_surface()
+new_obstacle_surf.set_colorkey((0,0,0))
 
 # filling in the surface with a color
-#surf_w,surf_h = new_obstacle_surf.get_size()
-#for x in range(surf_w):
-#    for y in range(surf_h):
-#        if new_obstacle_surf.get_at((x,y))[0] != 0:
-#            new_obstacle_surf.set_at((x,y),'orange')
+surf_w,surf_h = new_obstacle_surf.get_size()
+for x in range(surf_w):
+    for y in range(surf_h):
+        if new_obstacle_surf.get_at((x,y))[0] != 0:
+            new_obstacle_surf.set_at((x,y),'orange')
 
 
 while True:
@@ -28,14 +28,25 @@ while True:
             sys.exit()
 
     screen.fill('grey')
+
+    # complex way to create a mask outline
+    offset = 10
+    screen.blit(new_obstacle_surf,(obstacle_pos[0] + offset, obstacle_pos[1])) # right
+    screen.blit(new_obstacle_surf,(obstacle_pos[0] - offset, obstacle_pos[1])) # left
+    screen.blit(new_obstacle_surf,(obstacle_pos[0], obstacle_pos[1] - offset)) # top
+    screen.blit(new_obstacle_surf,(obstacle_pos[0], obstacle_pos[1] + offset)) # bottom
+    screen.blit(new_obstacle_surf,(obstacle_pos[0] + offset, obstacle_pos[1] - offset)) # topright
+    screen.blit(new_obstacle_surf,(obstacle_pos[0] - offset, obstacle_pos[1] - offset)) # topleft
+    screen.blit(new_obstacle_surf,(obstacle_pos[0] + offset, obstacle_pos[1] + offset)) # bottomright
+    screen.blit(new_obstacle_surf,(obstacle_pos[0] - offset, obstacle_pos[1] + offset)) # bottomleft
     screen.blit(obstacle_surf,obstacle_pos)
-    #screen.blit(new_obstacle_surf,obstacle_pos)
+    
 
     # a simple way to create an outline from a mask
-    for point in obstacle_mask.outline():
-        x = point[0] + obstacle_pos[0]
-        y = point[1] + obstacle_pos[1]
-        pygame.draw.circle(screen, 'red', (x,y), 3)
+    #for point in obstacle_mask.outline():
+    #    x = point[0] + obstacle_pos[0]
+    #    y = point[1] + obstacle_pos[1]
+    #    pygame.draw.circle(screen, 'red', (x,y), 3)
 
 
     pygame.display.update()
